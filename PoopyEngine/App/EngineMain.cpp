8ca514403919fd72e580ef8782efd9b93cc19bc3
EuihyeonO <yuu5666@naver.com>
2025-03-29 20:53:47 +0900

    로그 색상 변경

--- PoopyEngine/App/EngineMain.cpp
+++ PoopyEngine/App/EngineMain.cpp
@@ -1,6 +1,8 @@
 #include "EngineMain.h"
+#include "PDebugBase.h"
 
 int EngineMain()
 {
+	PLOG("TEXT");
 	return 0;
 }
--- PoopyEngine/PoopyLib/PDebugBase.h
+++ PoopyEngine/PoopyLib/PDebugBase.h
@@ -3,14 +3,15 @@
 #include <string_view>
 #include <Windows.h>
 
-#define NORMAL_COLOR FOREGROUND_INTENSITY
+#define WHITE_COLOR FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY
+#define LOG_COLOR FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY
 #define WARNING_COLOR FOREGROUND_GREEN | FOREGROUND_RED
 #define ERROR_COLOR FOREGROUND_RED
 
 #define LOGBASE(_LogStr, _FileName, _FunctionName, _FunctionLine, _WaringType)	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), _WaringType);														\
 																				std::cout << _LogStr << " " << "[" << _FileName << "]" << "[" << _FunctionName << ":" << _FunctionLine << "]" << std::endl;	\
-																				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), NORMAL_COLOR);
+																				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), WHITE_COLOR);
 
-#define PLOG(_LogStr) LOGBASE(_LogStr, __FILE__, __FUNCTION__, __LINE__, NORMAL_COLOR);
+#define PLOG(_LogStr) LOGBASE(_LogStr, __FILE__, __FUNCTION__, __LINE__, LOG_COLOR);
 #define PLOG_WARNING(_LogStr) LOGBASE(_LogStr, __FILE__, __FUNCTION__, __LINE__, WARNING_COLOR);
 #define PLOG_ERROR(_LogStr) LOGBASE(_LogStr, __FILE__, __FUNCTION__, __LINE__, ERROR_COLOR);
