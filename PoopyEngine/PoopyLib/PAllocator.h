dd88809039be4536af9c13bbb5b54f657a6983e9
EuihyeonO <yuu5666@naver.com>
2025-03-29 20:44:57 +0900

    Allocator 코드 제거

--- PoopyEngine/PoopyLib/PAllocator.h
+++ PoopyEngine/PoopyLib/PAllocator.h
@@ -1,34 +1,25 @@
 #pragma once
+#include "memory"
 #include "PoopyLibBase.h"
 
 template<typename T>
 class PAllocator
 {
 public:
+	typedef T Value_Type;
+
+public:
 	T* Allocate(int32 _Size);
-	void DeAllocate(T* _StartPoint, int32 _Size);
-	void Construct(T* _ConstructPoint, const T& _ConstructData);
-	void Destroy(T* _DestroyPoint);
+	void DeAllocate(T* _StartPoint);
 private:
 };
 
 template<typename T>
 inline T* PAllocator<T>::Allocate(int32 _Size)
 {
-	return nullptr;
 }
 
 template<typename T>
-inline void PAllocator<T>::DeAllocate(T* _StartPoint, int32 _Size)
-{
-}
-
-template<typename T>
-inline void PAllocator<T>::Construct(T* _ConstructPoint, const T& _ConstructData)
-{
-}
-
-template<typename T>
-inline void PAllocator<T>::Destroy(T* _DestroyPoint)
+inline void PAllocator<T>::DeAllocate(T* _StartPoint)
 {
 }
